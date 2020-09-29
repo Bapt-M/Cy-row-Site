1dd69ccc5505da1e057bed779ede16740ae2a6cb
Bapt-M <62667802+Bapt-M@users.noreply.github.com>
2020-09-29 19:31:02 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,3 +13,44 @@ $(document).ready(function(){
 });
 
 
+window.addEventListener("DOMContentLoaded", function () {
+
+  var form = document.getElementById("init-day");
+  // var button = document.getElementById("my-form-button");
+  var status = document.getElementById("status");
+
+  // Ajoute class "success" en cas de succes
+
+  function success() {
+    form.reset();
+    status.classList.add("success");
+    status.innerHTML = "Merci! :)";
+  }
+
+
+
+  form.addEventListener("submit", function (ev) {
+    ev.preventDefault();
+    var data = new FormData(form);
+    ajax(form.method, form.action, data, success, error);
+  });
+});
+
+// fonction "aidante" pour ajax
+
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
+}
+
+
